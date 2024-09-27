917503a5d4425f7efd74db123db319f7d955f67b
PDR'S store ⚡ <pdrstoreoficial@gmail.com>
2024-09-26 22:06:09 -0300

    Add files via upload

---
+++ scr.js
@@ -0,0 +1,7 @@
+document.querySelector(".contato").addEventListener("click", function() {
+    window.open("https://wa.me/5524998428760?text=Olá%20Pdr,%20quero%20atualizar%20meu%20guarda-roupa", "_blank");  });
+
+
+function produtoSelect(){
+    window.location.href = "/item001.html"
+}
